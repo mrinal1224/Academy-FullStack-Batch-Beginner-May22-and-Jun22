be39e1b3eb748db6ace1e0062db297b035930856
mrinal1224 <iammrinalkc@gmail.com>
2023-08-20 21:54:17 +0530

    push

--- Problem Solving Session/1.js
+++ Problem Solving Session/1.js
@@ -34,13 +34,69 @@
 
 //2 . Merge two sorted Arrays in Ascending Order and Remove Duplicates
 
-let arr1 = [1, 4, 6, 8]
-let arr2 = [2, 4, 6, 6, 8]
+// let arr1 = [1, 4, 6, 8]
+// let arr2 = [2, 4, 6, 6, 8]
 
-let arr3 = [...arr1 , ...arr2]
-arr3.sort((a ,b) => a-b)
-arr3 = [...new Set(arr3)]
-console.log(arr3)
+// let arr3 = [...arr1 , ...arr2]
+// arr3.sort((a ,b) => a-b)
+// arr3 = [...new Set(arr3)]
+// console.log(arr3)
+
+
+
+//3 
+
+
+// const user = {
+//     firstName: 'Mrinal',
+//     getName : function() {
+//       const firstName = 'Stevee!!';
+//       return this.firstName;
+//     }
+//   };
+//   console.log(user.getName()); // What is logged?
+
+
+//   4 . 
+  
+//   function makeUser() {
+//     return {
+//       name: "John",
+//       ref:function(){
+//         return (this.name)
+//       }
+//     };
+//   }
+
+//   let user = makeUser();
+
+//   console.log( user.ref()); // What's the result?
+
+
+// 5.
+
+let user = {    
+               name: 'Stevee', 
+               logMessage : function() { console.log(this.name); } 
+            }
+
+
+setTimeout(()=> {user.logMessage()}, 1000);
+
+
+
+// 6. 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
